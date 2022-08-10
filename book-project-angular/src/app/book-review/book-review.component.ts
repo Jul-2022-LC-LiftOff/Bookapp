32736558dace09e8fa2c19a15a1e572543edf293
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2022-08-09 20:56:07 -0500

    routing is set up
    why was that so hard, help me

---
+++ book-project-angular/src/app/book-review/book-review.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-review',
+  templateUrl: './book-review.component.html',
+  styleUrls: ['./book-review.component.scss']
+})
+export class BookReviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
