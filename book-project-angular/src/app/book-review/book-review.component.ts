3357aebbdb6be8c46e0688017763ae1fcddfcb6d
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2022-08-11 12:50:04 -0500

    update

--- book-project-angular/src/app/book-review/book-review.component.ts
+++ book-project-angular/src/app/book-review/book-review.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../data/book';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-book-review',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookReviewComponent implements OnInit {
 
-  constructor() { }
+  name = "test"
+  books: Array<Book> = [];
 
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
+
+
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.name = params['name'];
+    });
   }
+
+  getBook(search) {
+    let booksArr: Book[]
+    let searchTerm = search.searchTerm
+    let key = "AIzaSyAukQn7svQJN1ZruG8UK26I-LKr3lcEbGk"
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=${key}`)
+      .then(response => response.json())
+      .then(result => {
+        // console.log(result)
+        if (result.items.length > 0) {
+          for (let i = 0; i < result.items.length; i++) {
+            let data = result.items[i].volumeInfo
+            // console.log(data)
+            this.books[i] = data
+          }
+        } else {
+          console.log("Nothing to return.")
+        }
+      })
+  }
+
 
 }
