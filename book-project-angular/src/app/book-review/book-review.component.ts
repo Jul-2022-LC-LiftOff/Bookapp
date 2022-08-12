7462110d1b69917f93067e99c746e216960e64af
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2022-08-12 17:59:26 -0500

    update

--- book-project-angular/src/app/book-review/book-review.component.ts
+++ book-project-angular/src/app/book-review/book-review.component.ts
@@ -8,48 +8,43 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
   styleUrls: ['./book-review.component.scss']
 })
 export class BookReviewComponent implements OnInit {
-
+  
 
   books: Array<Book> = [];
+  similerbooks: Array<Book> = [];
   bookid;
   searchTerm;
   key = "AIzaSyAukQn7svQJN1ZruG8UK26I-LKr3lcEbGk";
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
+  test(){
+    console.log("test")
+  }
+
 
   ngOnInit() {
     this.bookid = this.activatedRoute.snapshot.paramMap.get('id');
-    console.log(this.bookid);
+    // console.log(this.bookid);
     fetch(`https://www.googleapis.com/books/v1/volumes/${this.bookid}?key=${this.key}`)
     .then(response => response.json())
     .then(result => {
-      console.log(result.volumeInfo)
-          this.books = result.volumeInfo
-          // console.log(data)
+      // console.log(result)
+          this.books[0] = result.volumeInfo
+          this.searchTerm = result.volumeInfo.authors
+          console.log(this.searchTerm)
     })  
-  }
-
-  
-
-  // getBook(search) {
-  //   let booksArr: Book[]
-  //   let searchTerm = search.searchTerm
-  //   let key = "AIzaSyAukQn7svQJN1ZruG8UK26I-LKr3lcEbGk"
-  //   fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=${key}`)
-  //     .then(response => response.json())
-  //     .then(result => {
-  //       // console.log(result)
-  //       if (result.items.length > 0) {
-  //         for (let i = 0; i < result.items.length; i++) {
-  //           let data = result.items[i].volumeInfo
-  //           // console.log(data)
-  //           this.books[i] = data
-  //         }
-  //       } else {
-  //         console.log("Nothing to return.")
-  //       }
-  //     })
-  // }
-
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${this.searchTerm}&key=${this.key}`)
+    .then(response => response.json())
+    .then(result => {
+      if (result.items.length > 0) {
+        for (let i = 0; i < result.items.length; i++) {
+          let data = result.items[i].volumeInfo;
+          let id = result.items[i].id;
+          this.similerbooks[i] = data;
+          this.similerbooks[i].id = id;
+        }
+    }
+  })
+}
 }
