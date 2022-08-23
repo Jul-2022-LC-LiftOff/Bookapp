866c875bffb759d76c8f0d26a56197ebd39b7d31
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2022-08-23 18:25:19 -0500

    update

--- book-project-angular/src/app/book-review/book-review.component.ts
+++ book-project-angular/src/app/book-review/book-review.component.ts
@@ -8,20 +8,29 @@ import { Router, ActivatedRoute, ParamMap, RouterLink } from '@angular/router';
   styleUrls: ['./book-review.component.scss']
 })
 export class BookReviewComponent implements OnInit {
-  
+
+  shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    return array;
+  }  
 
   books: Array<Book> = [];
   similerbooks: Array<Book> = [];
   bookid;
   searchTerm;
   key = "AIzaSyAukQn7svQJN1ZruG8UK26I-LKr3lcEbGk";
-
+  
   constructor(private activatedRoute: ActivatedRoute) { }
 
   reload(){
     setTimeout(() => {  window.location.reload(); }, 1);
   }
-
 
   ngOnInit() {
     this.bookid = this.activatedRoute.snapshot.paramMap.get('id');
@@ -30,8 +39,8 @@ export class BookReviewComponent implements OnInit {
     .then(response => response.json())
     .then(result => {
           this.books[0] = result.volumeInfo
-          this.searchTerm = result.volumeInfo.categories
-          console.log(result.volumeInfo.categories)
+          this.searchTerm = result.volumeInfo.authors
+          console.log(result.volumeInfo.imageLinks)
           fetch(`https://www.googleapis.com/books/v1/volumes?q=${this.searchTerm}&key=${this.key}`)
           .then(response => response.json())
           .then(result => {
@@ -41,6 +50,7 @@ export class BookReviewComponent implements OnInit {
                 let id = result.items[i].id;
                 this.similerbooks[i] = data;
                 this.similerbooks[i].id = id;
+                this.similerbooks = this.shuffle(this.similerbooks);
               }
           }
         })
--- book-project-angular/src/app/login-modal/login-modal.component.ts
+++ book-project-angular/src/app/login-modal/login-modal.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { getDatabase, ref, set, child, get } from "firebase/database";
+import { initializeApp } from 'firebase/app';
 
 @Component({
   selector: 'login-modal',
@@ -13,14 +15,48 @@ export class LoginModalComponent implements OnInit {
   loginFailed: boolean;
 
   // temporary credentials:
-  password: string = "12345"
-  username: string = "will"
+  password: string = "testpassword"
+  username: string = "testname"
 
   //display controls for validation:
   invalidPassword: boolean;
   invalidUsername: boolean;
 
+  userId = 1;
+
   constructor() { }
+
+  firebaseConfig = {
+    apiKey: "AIzaSyA970gnrfL9NNTRHm8Pj-HHiayCVQD0GZo",
+    authDomain: "book-app-d90d1.firebaseapp.com",
+    databaseURL: "https://book-app-d90d1-default-rtdb.firebaseio.com",
+    projectId: "book-app-d90d1",
+    storageBucket: "book-app-d90d1.appspot.com",
+    messagingSenderId: "1019367376762",
+    appId: "1:1019367376762:web:c759cb22843116796df8c3"
+  };
+  
+  app = initializeApp(this.firebaseConfig);
+
+  database = getDatabase(this.app);
+
+  writeUserData(name, password) {
+    const db = getDatabase();
+    set(ref(db, 'users/' + this.userId), {
+      username: name,
+      password: password
+    });
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `users/${this.userId}`)).then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
 
 
   ngOnInit(): void {
@@ -33,23 +69,25 @@ export class LoginModalComponent implements OnInit {
 
   onClickSubmit(data: { username: string, password: string }) {
     // console.log(data.username, data.password)
-    if (data.username == this.username && data.password == this.password) {
-      this.invalidPassword = false
-      this.invalidUsername = false
-      this.loggedIn.emit(true);
-      this.onCloseModal()
-    } else if (data.password != this.password && data.username == this.username) {
-      this.invalidPassword = true
-      this.invalidUsername = false
-      this.loggedIn.emit(false)
-    } else if (data.username != this.username && data.password == this.password) {
-      this.invalidUsername = true
-      this.invalidPassword = false
-      this.loggedIn.emit(false)
-    } else {
-      this.invalidPassword = true
-      this.invalidUsername = true
-      this.loggedIn.emit(false)
-    }
+    // if (data.username == this.username && data.password == this.password) {
+    //   this.invalidPassword = false
+    //   this.invalidUsername = false
+    //   this.loggedIn.emit(true);
+    //   this.onCloseModal()
+    // } else if (data.password != this.password && data.username == this.username) {
+    //   this.invalidPassword = true
+    //   this.invalidUsername = false
+    //   this.loggedIn.emit(false)
+    // } else if (data.username != this.username && data.password == this.password) {
+    //   this.invalidUsername = true
+    //   this.invalidPassword = false
+    //   this.loggedIn.emit(false)
+    // } else {
+    //   this.invalidPassword = true
+    //   this.invalidUsername = true
+    //   this.loggedIn.emit(false)
+    // }
+    this.userId++;
+    this.writeUserData(this.username, this.password)
   }
 }
