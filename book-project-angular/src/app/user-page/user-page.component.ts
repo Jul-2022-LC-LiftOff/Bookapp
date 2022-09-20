b9015c6ea475faaba16bf9ad2971c202e8fddbc2
MattRoski <mattroski88@gmail.com>
2022-09-19 20:13:27 -0500

    minor ui change

--- book-project-angular/src/app/user-page/user-page.component.ts
+++ book-project-angular/src/app/user-page/user-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { doc, getDoc, getFirestore, setDoc } from 'firebase/firestore';
 
 @Component({
   selector: 'app-user-page',
@@ -6,6 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-page.component.scss']
 })
 export class UserPageComponent implements OnInit {
+
+  // firebaseConfig = {
+  //   apiKey: "AIzaSyA970gnrfL9NNTRHm8Pj-HHiayCVQD0GZo",
+  //   authDomain: "book-app-d90d1.firebaseapp.com",
+  //   databaseURL: "https://book-app-d90d1-default-rtdb.firebaseio.com",
+  //   projectId: "book-app-d90d1",
+  //   storageBucket: "book-app-d90d1.appspot.com",
+  //   messagingSenderId: "1019367376762",
+  //   appId: "1:1019367376762:web:c759cb22843116796df8c3"
+  // };
+
+  
+  // key = "AIzaSyAukQn7svQJN1ZruG8UK26I-LKr3lcEbGk";
+  // app = initializeApp(this.firebaseConfig);
+  // auth = getAuth();
+  // db = getFirestore(this.app);
+  // user = this.auth.currentUser;
+  // userId = this.user.uid;
+ 
 
   constructor() { }
 
