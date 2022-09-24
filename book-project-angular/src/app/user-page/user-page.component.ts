51c4bf2f8e0a55b9f10b8f378be7a93d8c293ec0
MattRoski <mattroski88@gmail.com>
2022-09-24 17:07:12 -0500

    User page shows display name and email

--- book-project-angular/src/app/user-page/user-page.component.ts
+++ book-project-angular/src/app/user-page/user-page.component.ts
@@ -11,23 +11,23 @@ import { currentUser } from '../data/userData';
 })
 export class UserPageComponent implements OnInit {
 
-  // firebaseConfig = {
-  //   apiKey: "AIzaSyA970gnrfL9NNTRHm8Pj-HHiayCVQD0GZo",
-  //   authDomain: "book-app-d90d1.firebaseapp.com",
-  //   databaseURL: "https://book-app-d90d1-default-rtdb.firebaseio.com",
-  //   projectId: "book-app-d90d1",
-  //   storageBucket: "book-app-d90d1.appspot.com",
-  //   messagingSenderId: "1019367376762",
-  //   appId: "1:1019367376762:web:c759cb22843116796df8c3"
-  // };
+  firebaseConfig = {
+    apiKey: "AIzaSyA970gnrfL9NNTRHm8Pj-HHiayCVQD0GZo",
+    authDomain: "book-app-d90d1.firebaseapp.com",
+    databaseURL: "https://book-app-d90d1-default-rtdb.firebaseio.com",
+    projectId: "book-app-d90d1",
+    storageBucket: "book-app-d90d1.appspot.com",
+    messagingSenderId: "1019367376762",
+    appId: "1:1019367376762:web:c759cb22843116796df8c3"
+  };
 
 
-  // key = "AIzaSyAukQn7svQJN1ZruG8UK26I-LKr3lcEbGk";
-  // app = initializeApp(this.firebaseConfig);
-  // auth = getAuth();
-  // db = getFirestore(this.app);
-  // user = this.auth.currentUser;
-  // userId = this.user.uid;
+  key = "AIzaSyAukQn7svQJN1ZruG8UK26I-LKr3lcEbGk";
+  app = initializeApp(this.firebaseConfig);
+  auth = getAuth();
+  db = getFirestore(this.app);
+  user = this.auth.currentUser;
+  //userId = user.uid;
 
   username: string
   userImage: string
@@ -35,8 +35,10 @@ export class UserPageComponent implements OnInit {
     this.username = currentUser.name
     this.userImage = currentUser.image
   }
-
+  
   ngOnInit(): void {
+    // console.log(this.auth)
+    // console.log(this.auth.currentUser)
   }
 
 
